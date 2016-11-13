fb437de81fb550583248c63e95002c28bf859b3e
Holger Gruber <holger@gruber-frankfurt.de>
2016-11-13 23:53:32 +0100

    * added new device door/window sensor * process low level discovery every hour

--- homegear2zabbix.py
+++ homegear2zabbix.py
@@ -4,23 +4,28 @@ import paho.mqtt.client as mqtt
 import xmlrpclib
 import json
 from zabbix.sender import ZabbixMetric, ZabbixSender
+from datetime import datetime
 
 homegear_host = "balmung"
 zabbix_host = "balmung"
 application = "homegear"
+low_level_discovery_update_period = 3600
 types   = {
     "climate":  re.compile('^HM-WDS.*-TH-'), 
     "thermostat": re.compile('^HM-CC-RT-DN'),
-    "actor": re.compile('^HM-LC-Sw1-Pl-DN-R1')
+    "actor": re.compile('^HM-LC-Sw1-Pl-DN-R1'),
+    "door": re.compile('^HM-Sec-SCo')
 }
 sensors = {}
 devicetypes = {}
+last_update = datetime.now()
 
 def get_devices():
     devices = {}
     data = {}
     message = []
     homegear = xmlrpclib.ServerProxy('http://'+homegear_host+':2001')
+    print 'get devices from http://'+homegear_host+':2001:'
     for device in homegear.listDevices(False, ('FAMILY', 'ID', 'ADDRESS', 'TYPE', 'FIRMWARE')):
         info = homegear.getDeviceInfo(device['ID'], ('NAME', 'RSSI', 'INTERFACE'))
         name = info['NAME']
@@ -30,6 +35,7 @@ def get_devices():
         devices[devicetype].append({ '{#SENSOR}': name })
         sensors[int(device['ID'])] = name
         devicetypes[int(device['ID'])] = get_device_type(device['TYPE'])
+        print device['TYPE'] + ': ' + name
     for devicetype in devices:
         data['data'] = devices[devicetype]
         message.append(ZabbixMetric(zabbix_host, devicetype, json.dumps(data)))
@@ -44,6 +50,7 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("/#")
 
 def on_message(client, userdata, msg):
+    global last_update # modify global variable
     device = int(re.sub( r'.*/event/([0-9]*)/.*', '\\1', msg.topic))
     if device not in sensors:
         get_devices()
@@ -52,9 +59,14 @@ def on_message(client, userdata, msg):
     typ = devicetypes[device]
     parameter = re.sub( r'.*/event/.*/', '', msg.topic).lower()
     value = re.sub( r'\[(.*)\]', '\\1', msg.payload)
+    if value=='false': value=0
+    if value=='true': value=1
     message = []
     message.append(ZabbixMetric(zabbix_host, application+'.'+typ+'.'+parameter+'['+name+']', value))
     ZabbixSender(zabbix_host, 10051).send(message)
+    if ((datetime.now()-last_update).seconds > low_level_discovery_update_period):
+        last_update = datetime.now()
+        get_devices()
 
 get_devices()
 
